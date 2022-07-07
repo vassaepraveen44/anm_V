3e6a9075ce78fe1a2ff289229ed0c9d339a33613
praveen <praveenkapoor44@gmail.com>
2022-07-07 12:18:12 +0530

    uploading

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _width = 200;
+  double _height = 200;
+
+  double? _updateState() {
+    setState(() {
+      _width = 400;
+      _height = 400;
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: (() {
+                      _updateState();
+                    }),
+                    child: Text("Animation")),
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  width: _width,
+                  height: _height,
+                  color: Colors.lightBlue,
+                  child: Text(
+                    'animation',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
